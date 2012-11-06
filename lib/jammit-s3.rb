4fc7294302a1cd5f38dbb3d48ced4746d3f38658
Jon de Andres <jon@wuaki.tv>
2012-11-06 16:18:28 +0100

    Load cloudfront configuration from ROOT/config/cloudfront.yml

--- lib/jammit-s3.rb
+++ lib/jammit-s3.rb
@@ -6,5 +6,13 @@ module Jammit
   def self.upload_to_s3!(options = {})
     S3Uploader.new(options).upload
   end
-end
 
+  def self.cloudfront_configuration(env = nil)
+    @cloudfront_configuration ||=
+      begin
+        rails_env = defined?(Rails) ? Rails.env : ENV['RAILS_ENV']
+        cloudfront_config_path = File.join(ASSET_ROOT, 'config', 'cloudfront.yml')
+        cloudfront_config = symbolize_keys(YAML.load(File.read(cloudfront_config_path))[rails_env])
+      end
+  end
+end
